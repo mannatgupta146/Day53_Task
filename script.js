7dd4c05dff816240838b0720efdb8f1a06a197b5
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-28 00:11:09 +0530

    working

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let h2 = document.querySelector('h2');
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const text = h2.innerText
 
+h2.addEventListener('mouseenter', () => {
+    setInterval(() =>{
+        const str = text.split('').map((char,index) => {
+            return characters.split('')[Math.floor(Math.random() * 63)]
+        }).join('')
+
+        h2.innerText = str
+        
+    },100)
+})
