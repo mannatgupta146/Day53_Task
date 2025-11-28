513a103361e4c9d05ae96387bc5b76795696c84b
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-28 19:16:09 +0530

    logic making

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let h2 = document.querySelector('h2');
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 const text = h2.innerText
 
+let iterations = 0
+
 h2.addEventListener('mouseenter', () => {
-    setInterval(() =>{
+    if(iterations>char)
+    hi = setInterval(() =>{
         const str = text.split('').map((char,index) => {
             return characters.split('')[Math.floor(Math.random() * 63)]
         }).join('')
@@ -11,4 +14,9 @@ h2.addEventListener('mouseenter', () => {
         h2.innerText = str
         
     },100)
+})
+
+h2.addEventListener('mouseleave', () => {
+    h2.innerText = text
+    clearInterval(hi)
 })
